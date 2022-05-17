7e4b787609b0c3bdbed80a68002f102e0c8cacfd
julianaceciliabueno <juka.bueno@gmail.com>
2022-05-17 20:55:29 -0300

    teste de api com pyteste e requests

---
+++ tests/api/test_pet.py
@@ -0,0 +1,31 @@
+#bibliotecas
+import pytest
+import requests
+
+#definições das funções
+#variaveis publicas
+url = "https://petstore.swagger.io/v2/pet"
+header = {"Content-Type": "aplication/jason"}
+
+
+def teste_incluir_pet():
+    #prepara
+    # Dados de entrada via Json
+
+    #Resultados esperados
+    status_code_esperado = 200
+    pet_id_esperado = 6033070
+    pet_name_esperado = "Pipoca"
+    pet_nome_categoria_esperado = "cadela"
+    pet_nome_tag_esperado = "vacinada"
+
+    #executa
+    resultado_obtido = requests.post(
+        url=url,
+        headers=header,
+        data=open('C:\\Users\\julia\\PycharmProjects\\134inicial\\vendors\\json\\pet1.json')
+    )
+
+    #valida
+
+    print(resultado_obtido)
--- tests/unity/test_main.py
+++ tests/unity/test_main.py
@@ -1,5 +1,25 @@
-from main import somar, dividir, subtrair
+import csv
 
+import pytest
+
+from main import somar, dividir, subtrair, imprimir_oi
+# se um dia for mexer com o banco de dados, pedir uma view somente leitura
+
+
+#as funcões vem antes dos testes, pois podem seer usadas mais de uma vez
+def ler_csv(arquivo_csv):
+    dados_csv = [] #caixa vazia
+    try:
+        with open(arquivo_csv, newline='') as massa: #abriu o arquivo, newline ignora o titulo da tabela
+            campos = csv.reader(massa, delimiter=',') #dividiu em pedaços
+            next(campos)#muda para as proximas linhas
+            for linha in campos:
+                dados_csv.append(linha) #está organizando as linhas
+        return dados_csv #retorna com a lista montada
+    except FileExistsError: #se der errado tudo
+        print(f'arquivo não encontrado: {arquivo_csv}')
+    except Exception as fail: #para outras falhas genericas
+        print(f'falha imprevista: {fail}') #$ significa que virá uma variavel
 
 def somar(numero_a, numero_b):
     return numero_a + numero_b
@@ -34,9 +54,34 @@ def teste_somar():
     assert resultado_obtido == resultado_esperado
 
 
+def teste_subtrair():
+        # 1- configura (o que vc acha que vai dar? ex: resultado 15)
+
+    numero_a = 8
+    numero_b = 7
+    resultado_esperado = 1
+
+        # 2- executa   (chamar função colocando os valores ex: resultado_obtido = somar(numero_a, numero_b)
+
+    resultado_obtido = subtrair(numero_a, numero_b)
+
+        # 3- Valida    (verificar se o resultado esperado apareceu, se acertou passou, se não deu bug Ex: assert resultado_esperado == resultado_obtido)
+
+    assert resultado_obtido == resultado_esperado
+#================================================================================================
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado',ler_csv('C:\\Users\\julia\\PycharmProjects\\134inicial\\vendors\\CSV\\massa_teste_subtrair_positivo.csv'))
+def teste_subtrair_leitura_csv(numero_a, numero_b, resultado_esperado):
+    # 1 - Prepara / Configura
+
+    # 2 - Executa
+    resultado_obtido = subtrair(int(numero_a), int(numero_b))
+
+    # 3 - Valida / Checa
+    assert resultado_obtido == int(resultado_esperado)
+
 def teste_dividir():
     # 1- Configura
-    # 1.1- Dados de entrada
+
     numero_a = 27
     numero_b = 3
     # 1.2- Resultados esperados
@@ -48,6 +93,35 @@ def teste_dividir():
 
     #3- Valida
     assert resultado_obtido == resultado_esperado
-teste
 
-teste denovo
+#==============================================================================================================
+     #tem colchete pois é como se fosse uma tabela------- a tupla é a lista.
+lista_de_valores = [
+    (8, 7, 15),
+    (9, 9, 18),
+
+    (7, 7, 14)#não vai virgula no ultimo
+]
+    #1- configura (utilizamos a lista como massa de teste)
+    #parametrize= lê os dados e informar da onde os dados vêm
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', lista_de_valores)
+def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
+
+    #2- executa
+
+    resultado_obtido = somar(numero_a, numero_b)
+
+    #3- Valida
+
+    assert resultado_obtido == resultado_esperado
+#=============================================================================
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', ler_csv('C:\\Users\\julia\\PycharmProjects\\134inicial\\vendors\\CSV\\massa_teste_somar_positivo.csv'))
+def teste_somar_leitura_de_csv(numero_a, numero_b, resultado_esperado):
+
+    #2- executa
+
+    resultado_obtido = somar(int(numero_a), int(numero_b)) #int para transformar a string em numero
+
+    #3- Valida
+
+    assert resultado_obtido == int(resultado_esperado)#int para transformar a string em numero
